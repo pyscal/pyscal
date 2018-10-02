afcf0b7f65b280e7945eace41f24fcefdd7d731d
srmnitc <sarath.r.menon@gmail.com>
2018-10-02 12:31:28 +0200

    add structure identification files

---
+++ calculate_q4q6_histos.py
@@ -0,0 +1,146 @@
+"""
+Calculate the probability distribution to be used for identifying the structures
+Uses the steinhardt module to efficiently calculate q values.
+Takes one argument - name of the input trajectory file.
+By default calculates the q4,q6 space. However this can be easily modified by changing the variable qspace
+written by srm
+"""
+
+import steinhardt as st
+import numpy as np
+import sys
+import logging
+#import matplotlib.pyplot as plt
+##############################################################
+#nput to modify
+##############################################################
+qspace = [4,6]
+cutoff = 0.0001
+rcut   = 3.63
+histomin = 0.0
+histomax = 0.60
+histobins = 1000
+#cutoff <= sum(histo(n)-histo(n-1))/(number of non zero grids)
+##############################################################
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler("runlog.log")
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False  
+
+
+clogger = logging.getLogger('log1')
+handler = logging.FileHandler("convergence.dat")
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+clogger.addHandler(handler)
+clogger.setLevel(logging.DEBUG)
+clogger.propagate = False  
+#set up logger
+
+
+#get the infile
+infile = sys.argv[1]
+
+#checkfor dimension of qspace
+if len(qspace)!=2:
+    logger.error("only 2d qspace")
+    raise SystemExit()
+
+logger.info("infile name is %s"%infile)
+
+#convert the data from infile to systems
+systems = st.traj_to_systems(infile)
+
+#some histo params
+xaxis = np.linspace(histomin,histomax,histobins)
+
+#now create an oldhisto
+#cannot be normed at this point- we have to manually norm it later
+oldhisto,edgex,edgey = np.histogram2d([],[],bins=(xaxis,xaxis))
+
+#arrays for collecting the vals
+aq4=[]
+aq6=[]
+
+#now start calculating one by one
+converged=False
+for i in range(len(systems)):
+    systems[i].set_neighbordistance(rcut)
+    systems[i].set_reqd_qs(qspace)
+    systems[i].calculate_aq()
+
+    #collect the arrays
+    aq4 = np.concatenate((aq4,systems[i].gaqvals(qspace[0])))
+    aq6 = np.concatenate((aq6,systems[i].gaqvals(qspace[1])))
+
+    #now calculate the newhisto
+    newhisto,edgex,edgey = np.histogram2d(aq4,aq6,bins=(xaxis,xaxis))
+    #newhisto = oldhisto + newhistos
+    #norm both old and new histo
+    oldhistosum = np.sum(oldhisto)
+    newhistosum = np.sum(newhisto)
+
+    #create the normed histos
+    if oldhistosum>0:
+        normedold = oldhisto/float(oldhistosum)
+    else:
+        normedold = oldhisto
+    if newhistosum>0:
+        normednew = newhisto/float(newhistosum)
+    else:
+        normednew = newhisto
+
+    #print type(normednew)
+    #find the difference between old and new
+    diff = normednew-normedold
+    #diff = newhisto-oldhisto
+    #print diff
+    #print np.sum(diff)
+    #print "old sum is: %f new sum is %f"%(np.sum(normedold),np.sum(normednew))
+    #find number of non zero entries in new
+    nonzero = np.count_nonzero(normednew)
+
+    diffsum = np.sum(np.abs(diff))
+
+    diff_factor = diffsum/float(nonzero)
+
+    maxdiff = np.amax(np.abs(diff))
+
+    #print the diff factor
+    logger.info("diff after adding %d systems is %f and sum is %f, max diff is %f"%(i+1,diff_factor,diffsum,maxdiff))
+    clogger.info("%d %f %f %f"%(i+1,diff_factor,diffsum,maxdiff))
+    #now update the infos
+    oldhisto=newhisto
+
+    if (diffsum<=cutoff):
+        logger.info("histograms are converged after %d systems"%i+1)
+        converged=True
+        break
+
+#now lets test this much
+#once done, create histo and save the data
+if not converged:
+    logger.info("Did not achieve the required convergence cutoff of %f"%cutoff)
+#save as np array format
+outfile = infile+".histo.npy"
+np.save(outfile,normednew)
+logger.info("saved npy format as %s"%outfile)
+
+#save in human readable format
+#xmesh,ymesh = np.meshgrid(xaxis-1,xaxis-1)
+#plt.contourf(xmesh,ymesh,newhisto)
+summ = 0
+outfile = infile+".histo.dat"
+with open(outfile,'w') as fout:
+    for i in range(histobins-1):
+        for j in range(histobins-1):
+            if normednew[i,j]!=0:
+                summ+=normednew[i,j]
+                fout.write("%f %f %f\n"%(edgex[i],edgey[j],float(normednew[i,j])))
+
+logger.info("saved human readable histo as %s"%outfile)
+logger.info("convergence data is logged in convergence.dat")
+logger.info("added total of %f points, exiting.."%summ)
---
+++ find_structure_probs.py
@@ -0,0 +1,136 @@
+"""
+Read in the structure histograms of different structure -BCC,
+FCC,HCP and LQD.  
+"""
+from __future__ import print_function
+import steinhardt as st
+import numpy as np
+import sys
+import logging
+
+##############################################################
+#input to modify
+##############################################################
+bccfile = 'traj.bcc.histo.npy'
+fccfile = 'traj.fcc.histo.npy'
+hcpfile = 'traj.hcp.histo.npy'
+lqdfile = 'traj.lqd.histo.npy'
+qspace = [4,6]
+cutoff = 0.0001
+rcut   = 3.63
+histomin = 0.0
+histomax = 0.60
+histobins = 1000
+##############################################################
+
+#loggers
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler("runlog.log")
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False  
+
+clogger = logging.getLogger('log1')
+handler = logging.FileHandler("structure.dat")
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+clogger.addHandler(handler)
+clogger.setLevel(logging.DEBUG)
+clogger.propagate = False 
+
+clogger2 = logging.getLogger('log2')
+handler = logging.FileHandler("structure-non-normalised.dat")
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+clogger2.addHandler(handler)
+clogger2.setLevel(logging.DEBUG)
+clogger2.propagate = False 
+
+#get the infile
+filename = sys.argv[1]
+
+
+logger.info("infile name is %s"%filename)
+
+#convert the data from infile to systems
+#systems = st.traj_to_systems(infile)
+#some histo params
+xaxis = np.linspace(histomin,histomax,histobins)
+
+#read in the histofiles of various structures
+bcchisto = np.load(bccfile,mmap_mode='r')
+fcchisto = np.load(fccfile,mmap_mode='r')
+hcphisto = np.load(hcpfile,mmap_mode='r')
+lqdhisto = np.load(lqdfile,mmap_mode='r')
+
+#convert the data from infile to systems
+systems = st.traj_to_systems(filename)
+            
+for count,sys in enumerate(systems):
+    sys.set_neighbordistance(rcut)
+    sys.set_reqd_qs(qspace)
+    sys.calculate_aq()
+    #get q4 and q6 arrays
+    aq4=sys.gaqvals(qspace[0])
+    aq6=sys.gaqvals(qspace[1])
+    #get the bins of all the values to be binnerd
+    #aqs = np.stack((aq4,aq6),axis=-1)
+    #print type(aqs)
+    #print aqs.shape
+    #now find the bins of each values
+    netbcc=0
+    netfcc=0
+    nethcp=0
+    netlqd=0
+    for i in range(len(aq4)):
+        pair = [aq4[i],aq6[i]]
+        #binn = np.digitize(aq4[i],bins=(xaxis,xaxis))
+        newhisto,edgex,edgey = np.histogram2d([aq4[i]],[aq6[i]],bins=(xaxis,xaxis))
+        #print bcchisto[np.nonzero(newhisto)]
+        #newhistoint = (newhisto>0).astype(int)
+        bccprob = bcchisto[np.nonzero(newhisto)][0]
+        fccprob = fcchisto[np.nonzero(newhisto)][0]
+        hcpprob = hcphisto[np.nonzero(newhisto)][0]
+        lqdprob = lqdhisto[np.nonzero(newhisto)][0]
+        
+
+        #bccprob = bccprob[np.nonzero(bccprob)]
+        #print bccprob[np.nonzero(bccprob)]
+        #hcpprob = hcpprob[np.nonzero(hcpprob)]
+        #print hcpprob[np.nonzero(hcpprob)]
+        #fccprob = fccprob[np.nonzero(fccprob)]
+        #lqdprob = lqdprob[np.nonzero(lqdprob)]
+        #we need to correct the probabilities now
+        probsum = bccprob+fccprob+hcpprob+lqdprob
+        clogger2.info("%f %f %f %f"%(bccprob,fccprob,hcpprob,lqdprob))
+        bccprob/=probsum
+        fccprob/=probsum
+        hcpprob/=probsum
+        lqdprob/=probsum
+        #print the values
+        clogger.info("%f %f %f %f"%(bccprob,fccprob,hcpprob,lqdprob))
+
+        netbcc+=bccprob
+        netfcc+=fccprob
+        nethcp+=hcpprob
+        netlqd+=lqdprob
+
+    netbcc/=float(len(aq4))
+    netfcc/=float(len(aq4))
+    nethcp/=float(len(aq4))
+    netlqd/=float(len(aq4))
+
+    print("%d %f %f %f %f"%(count+1,netbcc,netfcc,nethcp,netlqd))
+
+    #newhisto,edgex,edgey = np.histogram2d(aq4,aq6,bins=(xaxis,xaxis))
+    #now get all nonzero bins of this histo
+    #newhistoint = (newhisto>0).astype(int)
+    #print newhistoint
+    #bccprob = newhistoint*bcchisto
+    #fccprob = newhistoint*fcchisto
+    #hcpprob = newhistoint*hcphisto
+    #lqdprob = newhistoint*lqdhisto
+    #finally print each prob and we are done?
+
