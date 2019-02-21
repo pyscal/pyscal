8055e32d66e8eb252059f1ed542e6651e63f697b
srmnitc <sarath.r.menon@gmail.com>
2019-02-21 16:48:44 +0100

    Add gzip support

--- src/steinhardt/traj_process.py
+++ src/steinhardt/traj_process.py
@@ -4,8 +4,10 @@ This file contains the methods for processing of a trajectory
 """
 from steinhardt import *
 import numpy as np
+import io
+import gzip
 
-def traj_to_systems(filename,natoms,snaps=False,npy=False):
+def traj_to_systems(filename,natoms,snaps=False,npy=False,zipped=False):
     """
     Reads in LAMMPS trajectory file and converts it to systems.
 
@@ -36,30 +38,38 @@ def traj_to_systems(filename,natoms,snaps=False,npy=False):
         snaps=True
 
     """
+    if zipped:
+        gz = gzip.open(filename,'rb')
+        infile = io.BufferedReader(gz)
+    else:
+        gz = open(filename,'r')
+        infile = gz        
 
     if snaps:
         nblock = natoms+9
         startblock = 0
         count=1
         files = []
-        with open(filename) as infile:
-            for line in infile:
-                if(count==1):
-                    ff = 'snap.'+str(startblock)+'.dat'
-                    files.append(ff)
-                    fout = open(ff,'w')
-                    fout.write(line)
-                    #count+=1
-                elif(count<nblock):
-                    fout.write(line)
-                    #count+=1
 
-                else:
-                    fout.write(line)
-                    fout.close()
-                    count=0
-                    startblock+=1
-                count+=1
+
+        for line in infile:
+            if(count==1):
+                ff = 'snap.'+str(startblock)+'.dat'
+                files.append(ff)
+                fout = open(ff,'w')
+                fout.write(line)
+                #count+=1
+            elif(count<nblock):
+                fout.write(line)
+                #count+=1
+
+            else:
+                fout.write(line)
+                fout.close()
+                count=0
+                startblock+=1
+            count+=1
+        infile.close()
 
         #qtraj = getQTrajectory(files)
         systems=[]
@@ -73,48 +83,49 @@ def traj_to_systems(filename,natoms,snaps=False,npy=False):
         atoms = []
         systems=[]
 
-        with open(filename,"r+") as infile:
-            iterval = infile
 
-            count = 1
-            timestep = 0
-            for line in iterval:
-                if count==6:
-                    box = []
-                    raw = line.strip().split()
-                    dimx = float(raw[1])-float(raw[0])          
+        iterval = infile
+        count = 1
+        timestep = 0
+        for line in iterval:
+            if count==6:
+                box = []
+                raw = line.strip().split()
+                dimx = float(raw[1])-float(raw[0])          
+            
+            elif count==7: 
+                raw = line.strip().split()
+                dimy = float(raw[1])-float(raw[0])
                 
-                elif count==7: 
-                    raw = line.strip().split()
-                    dimy = float(raw[1])-float(raw[0])
-                    
-                elif count==8:
-                    raw = line.strip().split()
-                    dimz = float(raw[1])-float(raw[0])
-                    boxd = [dimx,dimy,dimz]
-                    
-                elif count>9:
-                    line = line.strip().split()
-                    idd = int(line[0]) 
-                    x = float(line[3])
-                    y = float(line[4])
-                    z = float(line[5])
-                    xx = [x,y,z]
-                    a = Atom()
-                    a.sx(xx)
-                    a.sid(idd)  
-                    atoms.append(a)
-                    if count==natoms+9:
-                        count=0
-                        #do stuff here
-                        sys = System()
-                        sys.assign_particles(atoms,boxd)
-                        systems.append(sys)
-                        del atoms[:]
-                        del boxd[:]
-                        atoms = []
-                        boxd = []
-                count+=1
+            elif count==8:
+                raw = line.strip().split()
+                dimz = float(raw[1])-float(raw[0])
+                boxd = [dimx,dimy,dimz]
+                
+            elif count>9:
+                line = line.strip().split()
+                idd = int(line[0]) 
+                x = float(line[3])
+                y = float(line[4])
+                z = float(line[5])
+                xx = [x,y,z]
+                a = Atom()
+                a.sx(xx)
+                a.sid(idd)  
+                atoms.append(a)
+                if count==natoms+9:
+                    count=0
+                    #do stuff here
+                    sys = System()
+                    sys.assign_particles(atoms,boxd)
+                    systems.append(sys)
+                    del atoms[:]
+                    del boxd[:]
+                    atoms = []
+                    boxd = []
+            count+=1
+        infile.close()
+        
         if npy:
             np.save(".".join([filename,"npy"]),systems)
         return systems        
