b5e8ae99903ab7ad9b2115686ede6ac5b2f77773
srmnitc <sarath.menon@rub.de>
2019-08-03 23:22:51 +0200

    fix minor error in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-i# html_sidebars = {}
+# html_sidebars = {}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
