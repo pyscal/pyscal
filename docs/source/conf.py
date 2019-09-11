18f4bb82decebe8c99a444c0fe034945edb326f6
srmnitc <sarath.r.menon@gmail.com>
2019-09-11 14:05:30 +0200

    css style changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,6 +106,10 @@ html_theme_options = {
 
 }
 
+html_static_path = ['../_static']
+def setup(app):
+    app.add_stylesheet("default.css")
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
