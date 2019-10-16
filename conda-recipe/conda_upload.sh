8c6005d6323248ae68cdf04f831b6c7421d608a0
srmnitc <sarath.r.menon@gmail.com>
2019-10-16 15:02:58 +0200

    update osx convert

--- conda-recipe/conda_upload.sh
+++ conda-recipe/conda_upload.sh
@@ -9,5 +9,5 @@ export VERSION=`date +%Y.%m.%d`
 conda build .
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-*.tar.bz2 --force
 
-conda convert --platform osx-64 --force -o $CONDA_BLD_PATH/osx
+conda convert --platform osx-64  --force $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-*.tar.bz2 -o $CONDA_BLD_PATH/osx
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/osx/$PKG_NAME-`date +%Y.%m.%d`-*.tar.bz2 --force
