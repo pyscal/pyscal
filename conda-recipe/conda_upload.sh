fb9de747338d1fa20727f03ce01269eccb8e7289
srmnitc <sarath.r.menon@gmail.com>
2019-10-16 15:15:02 +0200

    change conda build recipe for osx

--- conda-recipe/conda_upload.sh
+++ conda-recipe/conda_upload.sh
@@ -10,4 +10,4 @@ conda build .
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-*.tar.bz2 --force
 
 conda convert --platform osx-64  --force $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-*.tar.bz2 -o $CONDA_BLD_PATH/osx
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/osx/* --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/osx/osx-64/* --force
