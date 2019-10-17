509692068400e03bbe6bc061222bdb894782ce91
srmnitc <sarath.r.menon@gmail.com>
2019-10-17 15:14:41 +0200

    add test for diamond

--- tests/test_crystal_structures.py
+++ tests/test_crystal_structures.py
@@ -3,12 +3,10 @@ import pyscal.crystal_structures as pcs
 import os
 
 def test_create_structure():
-    structure = ['bcc', 'fcc', 'hcp']
-    natoms = [2, 4, 4]
+    structure = ['bcc', 'fcc', 'hcp', 'diamond']
+    natoms = [2, 4, 4, 8]
 
     for count, struct in enumerate(structure):
         #dumpfile = os.path.join(os.getcwd(), "test.dat")
         atoms, boxdims = pcs.make_crystal(struct)
         assert len(atoms) == natoms[count]
-
-
