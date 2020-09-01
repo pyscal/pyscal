df15bd1bdb0e3704b059bd6d27dc5dc3deeb9e09
srmnitc <sarath.r.menon@gmail.com>
2020-09-01 18:02:28 +0200

    add covert ase method

--- src/pyscal/traj_process.py
+++ src/pyscal/traj_process.py
@@ -118,6 +118,79 @@ def read_ase(aseobject, check_triclinic=False, box_vectors=False):
     else:
         return atoms, box
 
+def convert_to_ase(sys, species=None):
+    """
+    Convert a given pyscal structure to ase object
+
+    Parameters
+    ----------
+    sys : System object
+        the system object to be converted
+
+    species : list of str
+        a list of species in the system
+
+    Returns
+    -------
+    aseobject: ASE atoms object
+
+    Notes
+    -----
+    ASE needs the species of atoms. If a property called `species`
+    exist in :attr:`~pyscal.catom.Atom.custom`, this value is used for
+    species. However if the value is not present, the keyword `species`
+    is required. This should contain a mapping between :attr:`~pyscal.catom.Atom.type`
+    and species name. For example, if `species` is `['Au', 'Ge']`, all atoms
+    of type 1 are assigned as Au and those of type 2 are assigned as Ge.
+    Note that ase is required to run this method.
+     
+    """
+    atoms = sys.atoms
+    #get element strings
+    if 'species' not in atoms[0].custom.keys():
+        if species is None:
+            raise ValueError("Species was not known! To convert to ase, species need to be provided using the species keyword")
+        #otherwise we know the species
+        types = [atom.type for atom in atoms]
+        unique_types = np.unique(types)
+        if not (len(unique_types) == len(species)):
+            raise ValueError("Length of species and number of types found in system are different")
+        #now assign the species to custom
+        for atom in atoms:
+            custom = atom.custom
+            custom['species'] = species[int(atom.type-1)]
+        #we should also get the unique species key
+        specieskey = "".join(species)
+    else:
+        #now if species are already there in custom
+        #we can safely ignore any input
+        types = [atom.type for atom in atoms]
+        unique_types = np.unique(types)
+        #now we know how many types are there
+        species = []
+        for ut in unique_types:
+            for atom in atoms:
+                if ut == atom.type:
+                    species.append(atom.custom['species'])
+                    break
+        specieskey = "".join(species)
+      
+    cell = sys.get_boxvecs()
+    pbc = [1, 1, 1]
+
+    #create ASE Atoms and assign everything
+    aseobject = Atoms()
+    aseobject.cell = cell
+    aseobject.pbc = pbc
+    
+    #thats everything pretty much
+    #now create ase Atom
+    for atom in atoms:
+        aseatom = Atom(atom.custom['species'], atom.pos)
+        aseobject.append(aseatom)
+    #done
+    return aseobject
+
 #functions that are not wrapped from C++
 def read_lammps_dump(infile, compressed = False, check_triclinic=False, box_vectors=False, customkeys=None):
     """
