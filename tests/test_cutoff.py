91733c9cd0c0eb120113b1436183820eebe18085
srmnitc <sarath.r.menon@gmail.com>
2020-09-10 11:26:12 +0200

    add test for reset cutoff method

---
+++ tests/test_cutoff.py
@@ -0,0 +1,15 @@
+import pytest
+import os
+import numpy as np
+import pyscal.core as pc
+import pyscal.crystal_structures as pcs
+
+def test_cutoff():
+    atoms, box = pcs.make_crystal(structure="fcc", lattice_constant=4.07, repetitions=(6,6,6))
+    sys = pc.System()
+    sys.atoms = atoms
+    sys.box = box
+    sys.find_neighbors(method="cutoff", cutoff=0)
+    sys.set_atom_cutoff(factor=2)
+    atoms = sys.atoms
+    assert (atoms[0].cutoff - 5.755849198858498) < 1E-5
