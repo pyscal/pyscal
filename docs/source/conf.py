473c7e8aaf229fa754ab975f54618269b6259fd0
srmnitc <sarath.r.menon@gmail.com>
2020-09-21 16:20:37 +0200

    Bump version: 2.8.12 → 2.8.13

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = u'2019, Sarath Menon, Grisell Díaz Leines, Jutta Rogal'
 author = u'Sarath Menon, Grisell Díaz Leines, Jutta Rogal'
 
 # The short X.Y version
-version = u'2.8.12'
+version = u'2.8.13'
 # The full version, including alpha/beta/rc tags
-release = u'2.8.12'
+release = u'2.8.13'
 
 
 # -- General configuration ---------------------------------------------------
