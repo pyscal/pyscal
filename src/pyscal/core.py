3be1f24ffa3677d1829a6dbf9627ff80268f158e
srmnitc <sarath.r.menon@gmail.com>
2020-11-17 17:12:31 +0100

    add embed method

--- src/pyscal/core.py
+++ src/pyscal/core.py
@@ -523,8 +523,17 @@ class System(pc.System):
                 raise RuntimeError("Could not find enough neighbors - try increasing threshold")
 
         elif method == 'voronoi':
+            
             self.voroexp = int(voroexp)
+
+            #copy the simulation cell
+            backupbox = self.box.copy()
+            if self.triclinic:
+                self.embed_in_cubic_box()
             self.get_all_neighbors_voronoi()
+
+            #replace box
+            self.box = backupbox
 
         self.neighbors_found = True
 
@@ -1910,6 +1919,46 @@ class System(pc.System):
         #print(len(completeatoms))
         return completeatoms
 
+
+    def embed_in_cubic_box(self,):
+        """
+        Embedded the triclinic box in a cubic box
+        """
+        #first task is to create a box representation
+        
+        box = self.box
+        backupbox = box.copy()
+        
+        a = np.array(box[0])
+        b = np.array(box[1])
+        c = np.array(box[2])
+
+        cosa = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+        cosb = np.dot(b, c)/(np.linalg.norm(b)*np.linalg.norm(c))
+        cosc = np.dot(c, a)/(np.linalg.norm(c)*np.linalg.norm(a))
+
+        lx = np.linalg.norm(a)
+        xy = np.linalg.norm(b)*cosc
+        xz = np.linalg.norm(c)*cosb
+        ly = np.sqrt(np.linalg.norm(b)*np.linalg.norm(b) - xy*xy)
+        yz = (np.linalg.norm(b)*np.linalg.norm(c)*cosa - xy*xz)/ly
+        lz = np.sqrt(np.linalg.norm(c)*np.linalg.norm(c) - xz*xz - yz*yz)
+
+        xlo = ylo = zlo = 0
+        xhi = lx
+        yhi = ly
+        zhi = lz
+
+        xlo_bound = xlo + min(0.0,xy,xz,xy+xz)
+        xhi_bound = xhi + max(0.0,xy,xz,xy+xz)
+        ylo_bound = ylo + min(0.0,yz)
+        yhi_bound = yhi + max(0.0,yz)
+        zlo_bound = zlo
+        zhi_bound = zhi
+
+        newbox = np.array([[xhi_bound-xlo_bound, 0, 0], [0, yhi_bound-ylo_bound, 0], [0, 0, zhi_bound-zlo_bound]])
+        
+        self.box = newbox
 
     def show(self, colorby=None, filterby=None):
         """
