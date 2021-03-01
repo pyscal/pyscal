3720d8a861d01ad1e5dc66b6a3605add29fbb41d
srmnitc <sarathmenon@mailbox.org>
2021-03-01 15:23:58 +0100

    Bump version: 2.10.10 → 2.10.11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,9 +32,9 @@ copyright = u'2019, Sarath Menon, Grisell Díaz Leines, Jutta Rogal'
 author = u'Sarath Menon, Grisell Díaz Leines, Jutta Rogal'
 
 # The short X.Y version
-version = u'2.10.10'
+version = u'2.10.11'
 # The full version, including alpha/beta/rc tags
-release = u'2.10.10'
+release = u'2.10.11'
 
 
 # -- General configuration ---------------------------------------------------
