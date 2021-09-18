9a5fba0014ef8e27a8e218f67df2fd9f0c5a1e33
Jan Janssen <jan.janssen@outlook.com>
2021-09-18 15:09:01 -0600

    Use pybind11 extension rather than cmake

--- setup.py
+++ setup.py
@@ -1,73 +1,5 @@
-import os
-import re
-import sys
-import sysconfig
-import platform
-import subprocess
-
-from distutils.version import LooseVersion
-from setuptools import setup, find_packages, Extension
-from setuptools.command.build_ext import build_ext
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-#checks for the cmake version
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                         out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        # Pile all .so in one place and use $ORIGIN as RPATH
-        cmake_args += ["-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE"]
-        cmake_args += ["-DCMAKE_INSTALL_RPATH={}".format("$ORIGIN")]
-
-        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                 cfg.upper(),
-                 extdir)]
-            build_args += ['--', '/m']
-        else:
-            build_args += ['--', '-j2']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                              cwd=self.build_temp)
-        print()  # Add an empty line for cleaner output
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_packages
 
 
 with open('README.rst') as readme_file:
@@ -86,9 +18,23 @@ setup(
     # and nowhere else
     package_dir={'':'src'},
     # add an extension module named 'python_cpp_example' to the package
-    ext_modules=[CMakeExtension('pyscal/catom'), CMakeExtension('pyscal/csystem')],
+    headers=["src/pyscal/atom.h", "src/pyscal/system.h", "lib/voro++/voro++.hh"],
+    ext_modules=[
+        Pybind11Extension(
+            "pyscal.catom",
+            ["src/pyscal/atom.cpp", "src/pyscal/atom_binding.cpp"],
+            language='c++',
+            include_dirs=['lib/voro++']
+        ),
+        Pybind11Extension(
+            "pyscal.csystem",
+            ["src/pyscal/system.cpp", "src/pyscal/system_binding.cpp", "src/pyscal/atom.cpp", "lib/voro++/voro++.cc"],
+            language='c++',
+            include_dirs=['lib/voro++']
+        ),
+    ],
     # add custom build_ext command
-    cmdclass=dict(build_ext=CMakeBuild),
+    cmdclass={"build_ext": build_ext},
     zip_safe=False,
     download_url = 'https://github.com/srmnitc/pyscal/archive/2.3.4.tar.gz',
     url = 'https://github.com/srmnitc/pyscal',
