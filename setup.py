2e722eb5e7624a9018c4a51f9bda32375d549cf5
Sarath Menon <sarathmenon@mailbox.org>
2021-10-17 16:35:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     zip_safe=False,
     download_url = 'https://anaconda.org/conda-forge/pyscal',
     url = 'https://pyscal.org',
-    install_requires=['numpy', 'ase', 'plotly'],
+    install_requires=['numpy', 'ase', 'plotly', 'ipywidgets'],
     classifiers=[
         'Programming Language :: Python :: 3'
     ]
