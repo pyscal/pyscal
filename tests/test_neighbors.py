9076de226eeee2ff2adaa5a4ec3b140c4fd14eae
srmnitc <sarath.r.menon@gmail.com>
2021-12-20 21:14:21 +0100

    add tests for neighbors

---
+++ tests/test_neighbors.py
@@ -0,0 +1,77 @@
+import pyscal.core as pc
+import os
+import pyscal.crystal_structures as pcs
+import numpy as np
+from ase.build import bulk
+
+def test_system_init():
+	atoms, box = pcs.make_crystal(structure='bcc', 
+                              lattice_constant=3.127, repetitions=(10,10,10),)
+	sys = pc.System()
+	sys.box = box
+	sys.atoms = atoms
+	sys.find_neighbors(method="cutoff", cutoff=3.6)
+	a1 = np.array(sys.neighbordist[0])
+	a2 = np.array([2.708061437633939,
+ 3.127,
+ 3.126999999999999,
+ 2.7080614376339383,
+ 3.127,
+ 3.126999999999999,
+ 2.7080614376339383,
+ 2.708061437633937,
+ 3.127,
+ 3.126999999999999,
+ 2.7080614376339383,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.7080614376339356])
+	assert np.sum(a1-a2) < 1E-5
+
+	sys.find_neighbors(method="cutoff", cutoff=3.6)
+	a1 = np.array(sys.neighbordist[0])
+	a2 = np.array([2.7080614376339356,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.708061437633939,
+ 3.126999999999999,
+ 3.126999999999999,
+ 3.126999999999999,
+ 3.127,
+ 3.127,
+ 3.127])
+	assert np.sum(a1-a2) < 1E-5
+
+	sys.find_neighbors(method="cutoff", cutoff='sann')
+	a1 = np.array(sys.neighbordist[0])
+	a2 = np.array([2.7080614376339356,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.708061437633939,
+ 3.126999999999999,
+ 3.126999999999999,
+ 3.126999999999999,
+ 3.127,
+ 3.127,
+ 3.127,
+ 4.422245809540667])
+	assert np.sum(a1-a2) < 1E-5
+
+	sys.find_neighbors(method="number", nmax=8)
+	a1 = np.array(sys.neighbordist[0])
+	a2 = np.array([2.7080614376339356,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.708061437633937,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.7080614376339383,
+ 2.708061437633939])
+	assert np.sum(a1-a2) < 1E-5
