8d943fd488dd9511d44b11921226bbe98b98dbfd
srmnitc <sarath.r.menon@gmail.com>
2022-04-29 10:49:00 +0200

    add test for unique voronoi vertices

--- tests/test_voronoi.py
+++ tests/test_voronoi.py
@@ -19,3 +19,13 @@ def test_voronoi_props():
 	assert (sys.atom.voronoi.volume[0]-15.288104691499992 < 1E-5)
 	assert (sys.atom.voronoi.face.perimeters[0][0]-6.6333687143110005 < 1E-5)
 	assert sys.atom.voronoi.face.vertices[0][0] == 6
+
+def test_voronoi_vertices():
+    nx = np.random.randint(1, 10)
+    nverts = (nx**3*12)
+    atoms, box = pcs.make_crystal(structure="bcc", repetitions=(nx,nx,nx))
+    sys = pc.System()
+    sys.box = box
+    sys.atoms = atoms
+    sys.find_neighbors(method='voronoi', cutoff=0.1)
+    assert len(sys.atom.voronoi.vertex.unique_positions) == nverts
