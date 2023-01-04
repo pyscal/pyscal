fix errors in neighbor distance calculation

src/pyscal/crystal_structures.py
@@ -86,9 +86,9 @@ def make_crystal(structure, lattice_constant = 1.00, repetitions = None, ca_rati
             for k in range(1, nz+1):
                 for l in range(1, sdict["natoms"]+1):
                     m += 1
-                    posx = (sdict["positions"][l-1][0]+(lattice_constant*sdict["scaling_factors"][0]*(float(i)-1)))
-                    posy = (sdict["positions"][l-1][1]+(lattice_constant*sdict["scaling_factors"][1]*(float(j)-1)))
-                    posz = (sdict["positions"][l-1][2]+(lattice_constant*sdict["scaling_factors"][2]*(float(k)-1)))
+                    posx = ((lattice_constant*sdict["positions"][l-1][0])+(lattice_constant*sdict["scaling_factors"][0]*(float(i)-1)))
+                    posy = ((lattice_constant*sdict["positions"][l-1][1])+(lattice_constant*sdict["scaling_factors"][1]*(float(j)-1)))
+                    posz = ((lattice_constant*sdict["positions"][l-1][2])+(lattice_constant*sdict["scaling_factors"][2]*(float(k)-1)))
                     if noise > 0:
                         posx = np.random.normal(loc=posx, scale=noise)
                         posy = np.random.normal(loc=posy, scale=noise)

tests/test_neighbors.py
@@ -27,10 +27,10 @@ def test_system_init():
  2.708061437633937,
  2.7080614376339356])
 	assert np.sum(a1-a2) < 1E-5
-	assert np.sum(sys.atom.neighbors.distance[0]-a2) < 1E-5
+	assert np.sum(sys.atoms.neighbors.distance[0]-a2) < 1E-5
 
 	sys.find_neighbors(method="cutoff", cutoff=3.6)
-	a1 = np.array(sys.neighbordist[0])
+	a1 = np.array(sys.atoms.neighbors.distance[0])
 	a2 = np.array([2.7080614376339356,
  2.708061437633937,
  2.708061437633937,
@@ -48,7 +48,7 @@ def test_system_init():
 	assert np.sum(a1-a2) < 1E-5
 
 	sys.find_neighbors(method="cutoff", cutoff='sann')
-	a1 = np.array(sys.neighbordist[0])
+	a1 = np.array(sys.atoms.neighbors.distance[0])
 	a2 = np.array([2.7080614376339356,
  2.708061437633937,
  2.708061437633937,
@@ -66,7 +66,7 @@ def test_system_init():
 	assert np.sum(a1-a2) < 1E-5
 
 	sys.find_neighbors(method="number", nmax=8)
-	a1 = np.array(sys.neighbordist[0])
+	a1 = np.array(sys.atoms.neighbors.distance[0])
 	a2 = np.array([2.7080614376339356,
  2.708061437633937,
  2.708061437633937,