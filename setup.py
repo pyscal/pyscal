2de923baf6b886ca3e356f38d1b8c4e4197d8d1a
Sarath Menon <sarathmenon@mailbox.org>
2023-02-22 17:35:29 +0100

    Bump version: 2.10.15 → 3.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='pyscal',
-    version='2.10.15',
+    version='3.0.0',
     author='Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Python library written in C++ for calculation of local atomic structural environment',
